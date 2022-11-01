3acf117d91301cbfadd4657f40e47acae73695ca
toasahi <asa1313to@gmail.com>
2022-11-01 13:37:41 +0900

    update:ガチャSVGを追加

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
 class IndexPage extends StatelessWidget {
@@ -7,28 +8,59 @@ class IndexPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0xFFDDEEF2),
-              Color(0xFFADE1E9),
-              Color(0xFF77DEEC),
-            ],
-            stops: [
-              0.0001,
-              0.412285,
-              1,
-            ],
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFFDDEEF2),
+                  Color(0xFFADE1E9),
+                  Color(0xFF77DEEC),
+                ],
+                stops: [
+                  0.0001,
+                  0.412285,
+                  1,
+                ],
+              ),
+            ),
+            width: double.infinity,
+            height: double.infinity,
+            child: SvgPicture.asset(
+              'assets/svg_images/indexBg.svg',
+            ),
           ),
-        ),
-        width: double.infinity,
-        height: double.infinity,
-        child: SvgPicture.asset(
-          'assets/svg_images/indexBg.svg',
-        ),
+          Center(
+            child: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 112.h),
+                  SvgPicture.asset(
+                    'assets/svg_images/gacha.svg',
+                    width: 254.w,
+                    height: 360.h,
+                    fit: BoxFit.scaleDown,
+                  ),
+                  SizedBox(height: 24.h),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24.w),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("test"),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
