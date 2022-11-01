d8e80c1a9c77eabb5dc02020bc6e596ebf4ee7ec
toasahi <asa1313to@gmail.com>
2022-11-01 11:55:36 +0900

    update:インデックページの背景追加

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'package:gacha_travel/pages/auth_page/auth_page.dart';
+import 'package:gacha_travel/pages/index_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData(fontFamily: 'NotoSansJP'),
-          home: const AuthPage(),
+          home: const IndexPage(),
         );
       },
     );
--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:gacha_travel/components/_components.dart';
+import 'package:flutter_svg/svg.dart';
 
 class IndexPage extends StatelessWidget {
   const IndexPage({super.key});
@@ -8,13 +7,29 @@ class IndexPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      top: false,
-      child: Column(
-        children: const [
-          GachaTravelAppBar(title: "index"),
-        ],
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFFDDEEF2),
+              Color(0xFFADE1E9),
+              Color(0xFF77DEEC),
+            ],
+            stops: [
+              0.0001,
+              0.412285,
+              1,
+            ],
+          ),
+        ),
+        width: double.infinity,
+        height: double.infinity,
+        child: SvgPicture.asset(
+          'assets/svg_images/indexBg.svg',
+        ),
       ),
-    ));
+    );
   }
 }
