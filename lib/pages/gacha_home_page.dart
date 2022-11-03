80b40cf0e6d74d93ed8ec8f76159620479f934f7
toasahi <asa1313to@gmail.com>
2022-11-03 10:30:40 +0900

    update:HomeページにbottomNavを追加

--- lib/pages/gacha_home_page.dart
+++ lib/pages/gacha_home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+
+import 'package:gacha_travel/constants.dart';
+import 'package:gacha_travel/pages/index_page.dart';
 
 class GachaHomePage extends StatelessWidget {
   const GachaHomePage({super.key});
@@ -27,28 +31,122 @@ class GachaHomePage extends StatelessWidget {
                 ],
               ),
             ),
-            width: double.infinity,
-            height: double.infinity,
             child: SvgPicture.asset(
               'assets/svg_images/gachaHome.svg',
+              height: double.infinity,
+              width: double.infinity,
+              fit: BoxFit.fill,
             ),
           ),
           SafeArea(
+            bottom: false,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                SizedBox(height: 8.h),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                Column(
                   children: [
-                    SvgPicture.asset(
-                      'assets/svg_images/appName.svg',
-                      fit: BoxFit.scaleDown,
+                    SizedBox(height: 8.h),
+                    Row(
+                      children: [
+                        SizedBox(width: 72.w),
+                        SvgPicture.asset(
+                          'assets/svg_images/appName.svg',
+                        ),
+                        SizedBox(width: 24.w),
+                        SizedBox(
+                          width: 28.w,
+                          height: 28.h,
+                          child: const Icon(
+                            Icons.settings,
+                          ),
+                        ),
+                        SizedBox(width: 16.w),
+                      ],
                     ),
                   ],
                 ),
+                Container(
+                  decoration: const BoxDecoration(
+                    color: AppColors.bottomNavigatorBgColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(37),
+                      topRight: Radius.circular(37),
+                    ),
+                  ),
+                  width: double.infinity,
+                  height: 80.h,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 48, vertical: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
+                          onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const IndexPage(),
+                            ),
+                          ),
+                          child: Column(
+                            children: [
+                              SvgPicture.asset(
+                                'assets/svg_images/bottomGacha.svg',
+                                width: 34.w,
+                                height: 34.h,
+                              ),
+                              Text(
+                                "ガチャ",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 9.sp,
+                                  color: AppColors.bottomNavigatorSelectedColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/svg_images/bottomCollection.svg',
+                              width: 34.w,
+                              height: 34.h,
+                            ),
+                            Text(
+                              "コレクション",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 9.sp,
+                                color: AppColors.bottomNavigatorTextColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/svg_images/bottomMypage.svg',
+                              width: 34.w,
+                              height: 34.h,
+                            ),
+                            Text(
+                              "マイページ",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 9.sp,
+                                color: AppColors.bottomNavigatorTextColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
