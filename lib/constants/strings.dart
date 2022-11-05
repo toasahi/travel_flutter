a4b5ae3c40b8c8bbaded9a77da76fcbfc882c2be
toasahi <asa1313to@gmail.com>
2022-11-05 14:18:38 +0900

    update:stringsファイル追加

---
+++ lib/constants/strings.dart
@@ -0,0 +1,10 @@
+class AppString {
+  static const String bgString = "";
+
+  static const String appBarBGString = "";
+  static const String appBarShadowString = "";
+  static const String cardBgString = "";
+
+  static const String importantTextString = "";
+
+}
