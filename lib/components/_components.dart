f4abb3662095617e6d542aca5f3e5fb67aa5e237
toasahi <asa1313to@gmail.com>
2022-11-05 15:18:29 +0900

    update:bottomページを追加

--- lib/components/_components.dart
+++ lib/components/_components.dart
@@ -1,3 +1,4 @@
 export 'package:gacha_travel/components/app_bar.dart';
 export 'package:gacha_travel/components/main_button.dart';
 export 'package:gacha_travel/components/sub_button.dart';
+export 'package:gacha_travel/components/bottom_nav.dart';
---
+++ lib/pages/collection_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'package:gacha_travel/importer.dart';
+
+class CollectionPage extends StatelessWidget {
+  const CollectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                SizedBox(height: 8.h),
+                Row(
+                  children: [
+                    SizedBox(width: 72.w),
+                    SvgPicture.asset(
+                      'assets/svg_images/appName.svg',
+                    ),
+                    SizedBox(width: 24.w),
+                    SizedBox(
+                      width: 28.w,
+                      height: 28.h,
+                      child: const Icon(
+                        Icons.settings,
+                      ),
+                    ),
+                    SizedBox(width: 16.w),
+                  ],
+                ),
+              ],
+            ),
+            const GachaTravelBottomNav(label: "collection"),
+          ],
+        ),
+      ),
+    );
+  }
+}
