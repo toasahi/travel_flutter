1518ec65a37919e7ea90c5a7762bc9ed3c106cb6
toasahi <asa1313to@gmail.com>
2022-11-08 11:42:50 +0900

    update:縦画面に固定

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app.dart';
 
 void main() {
+
+  // 画面の向き上部固定
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(const MyApp());
 }
